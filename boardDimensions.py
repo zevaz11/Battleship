def8930ae254873359dcda51c9bb6556449385d9
Liusss24 <162753550+Liusss24@users.noreply.github.com>
2024-04-17 19:43:02 -0600

    Versión_2
    12/04/2024

--- boardDimensions.py
+++ boardDimensions.py
@@ -1,31 +1,82 @@
-import sys
 from tkinter import *
 from tkinter import messagebox
-from tkinter import simpledialog
 def boardColumns ():
-  """ La función boardColumns, se encarga de generar y mostrar un input para que el usuario defina el numero de columnas del tablero
+  """ La función boardColumns, se encarga de obtener el valor ingresado para el numero de columnas y lo reasigna a otra variable para que pueda ser obtenido desde el archivo principal
   Returns:
-      int: el numero de columnas del tablero
+      scaleX: el numero de columnas del tablero
   """  
-  scaleX = int(simpledialog.askstring("Columnas", "Introduce la cantidad de columnas:"))
-  if scaleX < 20: #advertirle al usuario de que el numero de colunmas no puede ser menor a 20
-    messagebox.showinfo("Advertencia", "El numero de columnas no puede ser menor a 20")
-    sys.exit() # Esto es provisional, el código final no tiene que cerrarse si se le da un dato erroneo, en su lugar tendría que volver a abrir el input.
-  elif scaleX % 2 != 0: #advertirle al usuario de que el numero de columnas no puede ser impar
-    messagebox.showinfo("Advertencia", "El numero de columnas no puede ser impar")
-    sys.exit()
+  scaleX = valueX
   return scaleX
 
 def boardRows ():
-  """ La función boardRows, se encarga de generar y mostrar un input para que el usuario defina el numero de filas del tablero
+  """ La función boardRows, se encarga de obtener el valor ingresado para el numero de filas y lo reasigna a otra variable para que pueda ser obtenido desde el archivo principal
   Returns:
-      int: el numero de filas del tablero
+      scaleY: el numero de filas del tablero
   """
-  scaleY = int(simpledialog.askstring("Filas", "Introduce la cantidad de filas:"))
-  if scaleY < 10: #advertirle al usuario de que el numero de filas no puede ser menor a 10
-    messagebox.showinfo("Advertencia", "El numero de filas no puede ser menor a 10")
-    sys.exit() # // quitar despues
-  elif scaleY % 2 != 0: #advertirle al usuario de que el numero de filas no puede ser impar
-    messagebox.showinfo("Advertencia", "El numero de filas no puede ser impar")
-    sys.exit() # // quitar despues
+  scaleY = valueY
   return scaleY
+
+def dimensionsEntries():
+  """ La funcion DimensionsEntries, almacena y ejecuta el codigo necesario para mostrar los inputs con el tamaño del tablero
+  """
+  def checkEntry(text):
+    """ La función CheckEntry, revisa que el texto escrito en los inputs sean caracteres validos.
+    Args:
+        text (Entry): texto ingresado en los inputs
+    Returns:
+        bool: devuelve un falso en caso de que se escriba un dato no valido
+    """
+    if text.isdigit() or text == "":
+        return True
+    else:
+        messagebox.showerror("Error", "Solo se pueden colocar numeros enteros")
+        return False
+
+  def checkValues():
+    """ La función checkValues, revisa los datos ingresados y se asegura de que cumplan con todos los requisitos
+    """
+    intX = entryX.get()
+    intY = entryY.get()
+    if intX == "" or intY == "": #Revisa si alguno de los campos está vacio
+      messagebox.showerror("Error", "Uno de los campos está vacio.")
+
+    else:
+      global valueX
+      global valueY
+      valueX = int(intX)
+      valueY = int(intY)
+      if valueX < 20: #revisa que el valor de las columnas no sea menor a 20
+        messagebox.showerror("Error", "El numero de columnas no puede ser menor a 20")
+      elif valueY < 10: #revisa que el valor de las filas no sea menor a 10
+        messagebox.showerror("Error", "El numero de filas no puede ser menor a 10")
+      elif valueX % 2 != 0: #revisa que el valor de las columnas no sea impar
+        messagebox.showerror("Error", "El numero de columnas no puede ser impar")
+      elif valueY % 2 != 0: #revisa que el valor de las filas no sea impar
+        messagebox.showerror("Error", "El numero de filas no puede ser impar")
+      else:
+        messagebox.showinfo("Dimensiones", f"Las columnas miden {valueX} y las filas miden {valueY}")
+        inputWindow.destroy()
+
+  ##Ventana para el ingreso de los datos##
+  inputWindow = Tk()
+  inputWindow.title("Board Dimensions")
+
+  #Entry para las columnas
+  checked = inputWindow.register(checkEntry)
+  entryX = Entry(inputWindow, width="50", validate="key", validatecommand=(checked, "%P"))
+  entryX.pack(pady=10)
+
+  #Entry para las filas
+  checked = inputWindow.register(checkEntry)
+  entryY = Entry(inputWindow, width="50", validate="key", validatecommand=(checked, "%P"))
+  entryY.pack(pady=10)
+
+  #Boton para comprobar los valores.
+  continueButton = Button(inputWindow, text="Continuar", command=checkValues )
+  continueButton.pack(padx=5,pady=5)
+
+  inputWindow.mainloop()
+
+dimensionsEntries()
+boardColumns()
+boardRows()
--- main.py
+++ main.py
@@ -1,23 +1,64 @@
 from tkinter import *
+from keyboard import *
 from boardDimensions import * #llamada al archivo boardDimensions
 from PIL import ImageTk, Image
 
 buttonsMatrix=[]
+count = 0
 
+##Funciones KeyBinds##
+def rotateShip(event):
+  global count
+  if event.name == 'r' and event.event_type == KEY_DOWN:
+    count += 1
+  if count > 3:
+    count = 0
+  return count
+on_press_key("r", rotateShip) #// revisar porque no funciona.
+
+def selectShip1(event):
+  global shipImage
+  global count 
+  shipImage = Image.open("b1.png")
+  shipImage = shipImage.resize((50, 50))
+  shipImageRotated = shipImage.rotate(90*count)
+  shipImage = ImageTk.PhotoImage(shipImageRotated)
+  print("Seleccionaste el destructor")
+  return shipImage
+
+def selectShip2(event):
+  global shipImage
+  global count 
+  shipImage = Image.open("b22.png")
+  shipImage = shipImage.resize((50, 50))
+  shipImageRotated = shipImage.rotate(90*count)
+  shipImage = ImageTk.PhotoImage(shipImageRotated)
+  print("Seleccionaste el barco grande")
+  return shipImage
+
+def selectShip3(event):
+  global shipImage 
+  global count 
+  shipImage = Image.open("b33.png")
+  shipImage = shipImage.resize((50, 50))
+  shipImageRotated = shipImage.rotate(90*count)
+  shipImage = ImageTk.PhotoImage(shipImageRotated)
+  print("Seleccionaste el barco aún mas grande")
+  return shipImage
+
+# Colocar las naves //aun no se como funciona esta cosa
 def action(x,y):
   print (f"x={x},y={y}")
   global buttonsMatrix
   global shipImage
   buttonsMatrix[y][x].configure(image=shipImage)
-
+ 
 def board (x:int,y:int)->Tk:
   global buttonsMatrix 
   game = Tk()
   game.title("Piratas de Golfito")
   resolucion=f"{x*50}x{y*50}+0+0"
-
   game.geometry(resolucion)
-
   buttonsMatrix=[[Button(game, command=lambda x=c,y=f:action(x,y)) 
                    for c in range(x)] for f in range(y)]
   posx=0
@@ -31,10 +72,11 @@ def board (x:int,y:int)->Tk:
     posy+=50
   return game
 
-game=board(boardColumns(),boardRows())
+game = board(boardColumns(),boardRows())
 
-shipImage = Image.open("b1.png")
-shipImage = shipImage.resize((50, 50))  # Ajusta el tamaño de la shipImage
-shipImageRotated = shipImage.rotate(90)
-shipImage = ImageTk.PhotoImage(shipImageRotated)
+##KeyBinds##
+shipImage = game.bind("<KeyPress-q>", lambda event : selectShip1(event))
+shipImage = game.bind("<KeyPress-w>", lambda event : selectShip2(event))
+shipImage = game.bind("<KeyPress-e>", lambda event : selectShip3(event))
+
 game.mainloop()
